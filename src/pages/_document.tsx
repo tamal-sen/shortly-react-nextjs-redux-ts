a06fd72f94fcf13c49acd9539505457b68cbfec2
Tamal Sen <tamal.2k14@gmail.com>
2021-03-03 20:02:51 +0600

    added: custom server

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -75,10 +75,7 @@ export default class CustomDocument extends Document<CustomDocumentProps> {
             href="/images/favicon-32x32.png"
           />
           <link rel="shortcut icon" href="/images/favicon-32x32.png" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" />
         </Head>
         <body>
           <div dangerouslySetInnerHTML={{ __html: this.props.spriteContent }} />
